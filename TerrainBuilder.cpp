f923a127e9ba15fa43232096cdbb179afb5fcf9a
陈海月 <chenhaiyue@chenhaiyues-MacBook.local>
2021-03-23 00:19:30 -0400

    create cpp files for project.

---
+++ TerrainBuilder.cpp
@@ -0,0 +1,8 @@
+//
+//  TerrainBuilder.cpp
+//  
+//
+//  Created by Haiyue Chen on 3/16/21.
+//
+
+#include "TerrainBuilder.hpp"
---
+++ TerrainBuilder.hpp
@@ -0,0 +1,13 @@
+//
+//  TerrainBuilder.hpp
+//  
+//
+//  Created by Haiyue Chen on 3/16/21.
+//
+
+#ifndef TerrainBuilder_hpp
+#define TerrainBuilder_hpp
+
+#include <stdio.h>
+
+#endif /* TerrainBuilder_hpp */
