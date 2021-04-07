24a17cbcbab6ba1107340744e60cb803dbea83ca
haiyuechen131 <chen.213@wright.edu>
2021-04-06 23:48:48 -0400

    update my cpp and hpp and add traceability for req.15

--- TerrainBuilder.cpp
+++ TerrainBuilder.cpp
@@ -7,8 +7,13 @@
 
 #include "TerrainBuilder.hpp"
 
+void set_Block_Type(Block *block, int type);
  
-/// Ref: Req.15  The game shall have specific flower growing on blocks with specific textures.
-///
+int get_Block_Type();
+ 
+void set_Flower_Type(Flower *flower, int type)
 
+int get_Flower_Type()
+
+ 
 
--- TerrainBuilder.hpp
+++ TerrainBuilder.hpp
@@ -9,29 +9,44 @@
 #ifndef TerrainBuilder_hpp
 #define TerrainBuilder_hpp
 
-#define YELLOW_FLOWER
-#define RED_FLOWER
-#define PURPLE_FLOWER
-#define SUN_FLOWER
-#define WHITE_FLOWER
-#define BLUE_FLOWER
-#define BLOCK_01
-#define BLOCK_02
-#define BLOCK_03
-#define BLOCK_04
-#define BLOCK_05
-#define BLOCK_06
+#define YELLOW_FLOWER 0
+#define RED_FLOWER 1
+#define PURPLE_FLOWER 2
+#define SUN_FLOWER 3
+#define WHITE_FLOWER 4
+#define BLUE_FLOWER 5
+#define BLOCK_01 6
+#define BLOCK_02 7
+#define BLOCK_03 8
+#define BLOCK_04 9
+#define BLOCK_05 10
+#define BLOCK_06 11
 
-/// Ref: Req. 15  The game shall have specific flowers growing on blocks with specific textures.
-///
-///\param[in] type\imp\ref Req. 15  15.1type of block
-void set_Block_Type(int type);
+/// \imp \ref R16
+typedef struct{
+    int type;
+} Flower;
+/// \imp \ref R17
+typedef struct{
+    int type;
+} Block;
+ 
+ 
+/// \imp \ref R17
+/// \param[in] block\imp \ref R17 15.2 the type of block
+/// \param[in] type \imp \ref R17
+void set_Block_Type(Block *block, int type);
 
+/// \imp \ref R17
 int get_Block_Type();
 
-///\param[in] type\imp\ref Req. 15  15.2type of flower
-void set_Flower_Type(int type)
+/// \imp \ref R16
+/// \param[in] flower\imp \ref R16 15.1the type of  flower
+/// \param[in] type \imp \ref R16
+void set_Flower_Type(Flower *flower, int type)
 
+/// \imp \ref R16
 int get_Flower_Type()
 
+ 
 #endif /* TerrainBuilder_hpp */
