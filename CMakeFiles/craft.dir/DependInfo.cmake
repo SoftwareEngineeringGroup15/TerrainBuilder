8d2d9a54aea7e83b16734504801129f636fa3394
Alex Gentry <gentryaj@mail.uc.edu>
2021-04-28 23:21:33 -0400

    added cmake files

---
+++ CMakeFiles/craft.dir/DependInfo.cmake
@@ -0,0 +1,43 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/alex/Documents/TerrainBuilder/deps/glew/src/glew.c" "/home/alex/Documents/TerrainBuilder/CMakeFiles/craft.dir/deps/glew/src/glew.c.o"
+  "/home/alex/Documents/TerrainBuilder/deps/lodepng/lodepng.c" "/home/alex/Documents/TerrainBuilder/CMakeFiles/craft.dir/deps/lodepng/lodepng.c.o"
+  "/home/alex/Documents/TerrainBuilder/deps/noise/noise.c" "/home/alex/Documents/TerrainBuilder/CMakeFiles/craft.dir/deps/noise/noise.c.o"
+  "/home/alex/Documents/TerrainBuilder/deps/sqlite/sqlite3.c" "/home/alex/Documents/TerrainBuilder/CMakeFiles/craft.dir/deps/sqlite/sqlite3.c.o"
+  "/home/alex/Documents/TerrainBuilder/deps/tinycthread/tinycthread.c" "/home/alex/Documents/TerrainBuilder/CMakeFiles/craft.dir/deps/tinycthread/tinycthread.c.o"
+  "/home/alex/Documents/TerrainBuilder/src/auth.c" "/home/alex/Documents/TerrainBuilder/CMakeFiles/craft.dir/src/auth.c.o"
+  "/home/alex/Documents/TerrainBuilder/src/client.c" "/home/alex/Documents/TerrainBuilder/CMakeFiles/craft.dir/src/client.c.o"
+  "/home/alex/Documents/TerrainBuilder/src/cube.c" "/home/alex/Documents/TerrainBuilder/CMakeFiles/craft.dir/src/cube.c.o"
+  "/home/alex/Documents/TerrainBuilder/src/db.c" "/home/alex/Documents/TerrainBuilder/CMakeFiles/craft.dir/src/db.c.o"
+  "/home/alex/Documents/TerrainBuilder/src/item.c" "/home/alex/Documents/TerrainBuilder/CMakeFiles/craft.dir/src/item.c.o"
+  "/home/alex/Documents/TerrainBuilder/src/main.c" "/home/alex/Documents/TerrainBuilder/CMakeFiles/craft.dir/src/main.c.o"
+  "/home/alex/Documents/TerrainBuilder/src/map.c" "/home/alex/Documents/TerrainBuilder/CMakeFiles/craft.dir/src/map.c.o"
+  "/home/alex/Documents/TerrainBuilder/src/matrix.c" "/home/alex/Documents/TerrainBuilder/CMakeFiles/craft.dir/src/matrix.c.o"
+  "/home/alex/Documents/TerrainBuilder/src/ring.c" "/home/alex/Documents/TerrainBuilder/CMakeFiles/craft.dir/src/ring.c.o"
+  "/home/alex/Documents/TerrainBuilder/src/sign.c" "/home/alex/Documents/TerrainBuilder/CMakeFiles/craft.dir/src/sign.c.o"
+  "/home/alex/Documents/TerrainBuilder/src/util.c" "/home/alex/Documents/TerrainBuilder/CMakeFiles/craft.dir/src/util.c.o"
+  "/home/alex/Documents/TerrainBuilder/src/world.c" "/home/alex/Documents/TerrainBuilder/CMakeFiles/craft.dir/src/world.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "deps/glew/include"
+  "deps/glfw/include"
+  "deps/lodepng"
+  "deps/noise"
+  "deps/sqlite"
+  "deps/tinycthread"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/alex/Documents/TerrainBuilder/deps/glfw/src/CMakeFiles/glfw.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
