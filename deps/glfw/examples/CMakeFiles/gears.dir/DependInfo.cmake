5274d0fec962f179f223ea1b4f8a1c5700f29bec
Alex Gentry <gentryaj@mail.uc.edu>
2021-04-28 23:40:02 -0400

    added glfw

---
+++ deps/glfw/examples/CMakeFiles/gears.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/alex/Documents/TerrainBuilder/deps/glfw/examples/gears.c" "/home/alex/Documents/TerrainBuilder/deps/glfw/examples/CMakeFiles/gears.dir/gears.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "deps/glfw/include"
+  "deps/glfw/deps"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/alex/Documents/TerrainBuilder/deps/glfw/src/CMakeFiles/glfw.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
