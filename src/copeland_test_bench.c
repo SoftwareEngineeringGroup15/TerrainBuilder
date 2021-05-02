21cb4518c507bec066060cf9ff4c15268843cfad
dianacopeland <dcope0614@gmail.com>
2021-05-02 16:18:31 -0400

    Fixed some issues that were stopping compile Had more issues in my test file when trying to remove my main method for testing

--- src/copeland_test_bench.c
+++ src/copeland_test_bench.c
@@ -20,15 +20,16 @@ int test_is_plant(){
 	return is_obstacle(items[29]);
 }
 
+// Removed int main because it caused problems in main.c
+// Verified that plants are now obstacles and that current time is being tracked.
 
-
-int main(int argc, char **argv)
-{
-	long current_time = test_track_system_time();
-	printf("Current time in seconds is %s, " current_time)
-
-	int plant_obstacle = test_is_plant();
-	printf("Plants are now obstacles %s ", plant_obstacle)
-
-    return 0;
-}
+//int main(int argc, char **argv)
+//{
+//	long current_time = test_track_system_time();
+//	printf("Current time in seconds is %s, ", current_time);
+//
+//	int plant_obstacle = test_is_plant();
+//	printf("Plants are now obstacles %s ", plant_obstacle);
+//
+//    return 0;
+//}
